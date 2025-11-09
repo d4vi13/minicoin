86ab7680551d55630a5d0603f4e5062a47e2ec83
ferbcastro <fernandocuritiba3@gmail.com>
2025-11-09 00:00:12 -0300

    2-servidor-tcp new server methods

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,5 +1,26 @@
 package server
 
-func Start() {
+import (
+	"net"
+	"github.com/d4vi13/minicoin/internal/api"
+)
+
+func Serve(port int) error {
+	
+}
+
+func handleClient(connection *net.netConn) {
+	
+}
+
+func handleTransaction(clientId uint, value int64) (api.ServerResponse, error) {
+
+}
+
+func handleCheckBlockchain() (api.ServerResponse, error) {
+
+}
+
+func handleCheckBalance() (api.ServerResponse, error) {
 
 }
