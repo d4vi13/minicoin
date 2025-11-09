cd40182d1e45260bc9dbb67af577e68c69feb497
ferbcastro <fernandocuritiba3@gmail.com>
2025-11-08 21:56:42 -0300

    1-cliente-tcp first api type definitions

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
   "github.com/d4vi13/minicoin/internal/client"
+  "github.com/d4vi13/minicoin/internal/api"
 )
 
 func main() {
   client.Start()
+
+  var pkgTest api.Package
+  pkgTest.PkgType = api.ClientRequestPkg
 }
---
+++ internal/api/api.go
@@ -0,0 +1,54 @@
+package api
+
+type PackageType int
+
+const (
+	ClientRequestPkg PackageType = iota
+	ServerResponsePkg
+)
+
+type ServerResponseType int
+
+const (
+	ServerSuccessResponse ServerResponseType = iota
+	ServerFailedResponse
+)
+
+type ServerFailType int
+
+const (
+	ServerNoFail ServerFailType = iota
+	ServerClientUnkown
+	ServerClientOverdraw
+	ServerBlockchainTainted
+)
+
+type ClientRequestType int
+
+const (
+	ClientCheckBalance ClientRequestType = iota
+	ClientTransaction
+	ClientCheckBlockchainIntegrity
+)
+
+// Defines interface for communication
+type Package struct {
+	PkgType PackageType
+	PayloadSize int64
+	Payload []byte
+}
+
+// Defines interface for client request
+type ClientRequest struct {
+	Type ClientRequestType
+	Identifier int64
+	TransactionValue int64
+}
+
+// Defines interface for server response
+type ServerResponse struct {
+	Type ServerResponseType
+	FailType ServerFailType
+	ClientBalance int64
+}
+
