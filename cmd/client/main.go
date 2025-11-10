bad220c291a46d015a94ba7cd12d7e304c59d6d3
ferbcastro <fernandocuritiba3@gmail.com>
2025-11-10 18:27:03 -0300

    1-cliente-tcp Init implemented

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/d4vi13/minicoin/internal/client"
 	"log"
 	"strconv"
+
+	"github.com/d4vi13/minicoin/internal/client"
 )
 
-func Parse(addr *string, port *int, clientId *int, action *int, value *string) error {
+func Parse(addr *string, port *int, clientId *uint, action *int, value *string) error {
 	const (
 		ADDR_DEFAULT   = "localhost"
 		PORT_DEFAULT   = 8080
-		ID_DEFAULT     = -1
+		ID_DEFAULT     = 0
 		ACTION_DEFAULT = -1
 		VALUE_DEFAULT  = ""
 	)
@@ -23,21 +24,21 @@ func Parse(addr *string, port *int, clientId *int, action *int, value *string) e
 
 	flag.StringVar(addr, "addr", ADDR_DEFAULT, "define server address")
 	flag.IntVar(port, "port", PORT_DEFAULT, "define server port")
-	flag.IntVar(clientId, "id", ID_DEFAULT, "define client")
+	flag.UintVar(clientId, "id", ID_DEFAULT, "define client")
 	flag.IntVar(action, "action", ACTION_DEFAULT, actionDesc)
 	flag.StringVar(value, "value", VALUE_DEFAULT, "define transaction value")
 
 	flag.Parse()
 
 	if *clientId == ID_DEFAULT {
-		return fmt.Errorf("No id given!")
+		return fmt.Errorf("Invalid client id!")
 	}
 	if *action == -1 {
 		return fmt.Errorf("No action given!")
 	}
-  if *action >= client.MAX_ACTION {
-    return fmt.Errorf("Invalid action given!")
-  }
+	if *action >= client.MAX_ACTION {
+		return fmt.Errorf("Invalid action given!")
+	}
 	if *action == client.TRANSACTION && *value == VALUE_DEFAULT {
 		return fmt.Errorf("Transaction set and no value given!")
 	}
@@ -48,19 +49,19 @@ func Parse(addr *string, port *int, clientId *int, action *int, value *string) e
 func main() {
 	var addr string
 	var action int
-	var id int
-	var value int
+	var id uint
+	var value int64
 	var port int
 
 	var valueString string
 	err := Parse(&addr, &port, &id, &action, &valueString)
 	if err != nil {
 		flag.PrintDefaults()
-    fmt.Println()
+		fmt.Println()
 		log.Fatal(err)
 	}
 
-	value, err = strconv.Atoi(valueString)
+	value, err = strconv.ParseInt(valueString, 10, 64)
 	if err != nil {
 		log.Fatal(err)
 	}
